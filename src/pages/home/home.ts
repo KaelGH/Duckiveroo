6df3b3214e6941804abf023e79918723e65581ed
Kael Gustin-Hiron <thekingofpop98@hotmail.fr>
2018-10-19 17:29:50 +0200

    init projet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ComptePage } from './../compte/compte'
+import { RecherchePage } from './../recherche/recherche'
+import { RestosPage } from './../restos/restos'
+import { Component } from '@angular/core'
+import { NavController } from 'ionic-angular'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tab1: any
+  tab2: any
+  tab3: any
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
+    this.tab1 = RestosPage
+    this.tab2 = RecherchePage
+    this.tab3 = ComptePage
   }
-
 }
---
+++ src/pages/restos/restos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RestosPage } from './restos';
+
+@NgModule({
+  declarations: [
+    RestosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RestosPage),
+  ],
+})
+export class RestosPageModule {}
